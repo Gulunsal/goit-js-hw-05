e38bc5c7b7ac05c07e0ab927b2d8403de3791db8
gulunsal <gulayduzgun@hotmail.com>
2024-08-01 12:02:18 +0300

    hw-js 05 update consol error

--- js/task-4.js
+++ js/task-4.js
@@ -7,46 +7,48 @@ const getTotalBalanceByGender = (users, gender) => {
 //ilk önce filter metodu, users dizisini dolaşıp gender parametresi ile eşleşen cinsiyetteki kullanıcıları içeren yeni bir dizi oluşturuldu.
 //sonra reduce metodu, bu dizi uzerinden toplama islemi yapıldı.
 
-const allUsers = [
-  {
-    name: "Moore Hensley",
-    gender: "male",
-    balance: 2811
-  },
-  {
-    name: "Sharlene Bush",
-    gender: "female",
-    balance: 3821
-  },
-  {
-    name: "Ross Vazquez",
-    gender: "male",
-    balance: 3793
-  },
-  {
-    name: "Elma Head",
-    gender: "female",
-    balance: 2278
-  },
-  {
-    name: "Carey Barr",
-    gender: "male",
-    balance: 3951
-  },
-  {
-    name: "Blackburn Dotson",
-    gender: "male",
-    balance: 1498
-  },
-  {
-    name: "Sheree Anthony",
-    gender: "female",
-    balance: 2764
-  }
-];
+
+// allUsers değişkeni global olarak tanımlandığı için consol da hata aldım, bu nedenle yeniden tanımlanma yaptım. 
+if (typeof allUsers === 'undefined') {
+  var allUsers = [
+    {
+      name: "Moore Hensley",
+      gender: "male",
+      balance: 2811
+    },
+    {
+      name: "Sharlene Bush",
+      gender: "female",
+      balance: 3821
+    },
+    {
+      name: "Ross Vazquez",
+      gender: "male",
+      balance: 3793
+    },
+    {
+      name: "Elma Head",
+      gender: "female",
+      balance: 2278
+    },
+    {
+      name: "Carey Barr",
+      gender: "male",
+      balance: 3951
+    },
+    {
+      name: "Blackburn Dotson",
+      gender: "male",
+      balance: 1498
+    },
+    {
+      name: "Sheree Anthony",
+      gender: "female",
+      balance: 2764
+    }
+  ];
+}
 
 console.log(getTotalBalanceByGender(allUsers, "male")); // 12053
 console.log(getTotalBalanceByGender(allUsers, "female")); // 8863
-
-
 
