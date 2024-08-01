2910e3b6e9a36fddcf648e4890a7438b486e33f0
gulunsal <gulayduzgun@hotmail.com>
2024-08-01 12:44:34 +0300

    hw js 05 change update vercion 04

--- js/task-4.js
+++ js/task-4.js
@@ -1,54 +1,49 @@
-//mission 4-Total Balance by Gender
+//mission-4 - The Unique Names
 const getTotalBalanceByGender = (users, gender) => {
-  return users
-    .filter(user => user.gender === gender)
-    .reduce((total, user) => total + user.balance, 0);
+  return users.reduce((total, user) => {
+    return user.gender === gender ? total + user.balance : total;
+  }, 0);
 };
-//ilk önce filter metodu, users dizisini dolaşıp gender parametresi ile eşleşen cinsiyetteki kullanıcıları içeren yeni bir dizi oluşturuldu.
-//sonra reduce metodu, bu dizi uzerinden toplama islemi yapıldı.
 
 
-// allUsers değişkeni global olarak tanımlandığı için consol da hata aldım, bu nedenle yeniden tanımlanma yaptım. 
-if (typeof allUsers === 'undefined') {
-  var allUsers = [
-    {
-      name: "Moore Hensley",
-      gender: "male",
-      balance: 2811
-    },
-    {
-      name: "Sharlene Bush",
-      gender: "female",
-      balance: 3821
-    },
-    {
-      name: "Ross Vazquez",
-      gender: "male",
-      balance: 3793
-    },
-    {
-      name: "Elma Head",
-      gender: "female",
-      balance: 2278
-    },
-    {
-      name: "Carey Barr",
-      gender: "male",
-      balance: 3951
-    },
-    {
-      name: "Blackburn Dotson",
-      gender: "male",
-      balance: 1498
-    },
-    {
-      name: "Sheree Anthony",
-      gender: "female",
-      balance: 2764
-    }
-  ];
-}
+const allUsers = [
+	{
+    name: "Moore Hensley",
+    gender: "male",
+    balance: 2811
+  },
+  {
+    name: "Sharlene Bush",
+    gender: "female",
+    balance: 3821
+  },
+  {
+    name: "Ross Vazquez",
+    gender: "male",
+    balance: 3793
+  },
+  {
+    name: "Elma Head",
+    gender: "female",
+    balance: 2278
+  },
+  {
+    name: "Carey Barr",
+    gender: "male",
+    balance: 3951
+  },
+  {
+    name: "Blackburn Dotson",
+    gender: "male",
+    balance: 1498
+  },
+  {
+    name: "Sheree Anthony",
+    gender: "female",
+    balance: 2764
+  }
+];
 
 console.log(getTotalBalanceByGender(allUsers, "male")); // 12053
-console.log(getTotalBalanceByGender(allUsers, "female")); // 8863
 
+console.log(getTotalBalanceByGender(allUsers, "female")); // 8863
